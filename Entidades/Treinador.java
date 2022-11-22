310ddddbc49dc50c807949af2dc47aa51059ac38
MariaContu <112641497+MariaContu@users.noreply.github.com>
2022-11-22 19:27:57 -0300

    Update Treinador.java

--- Entidades/Treinador.java
+++ Entidades/Treinador.java
@@ -3,9 +3,22 @@ import Main.*;
 import Main.Enums.Raridade;
 
 public class Treinador extends Carta {
+    
+    private String descricao;
+    private String tipo;
 
-    public Treinador(String nome, Raridade raridade) {
+    public Treinador(String nome, Raridade raridade, String descricao, String tipo) {
         super(nome, raridade);
+        this.descricao=descricao;
+        this.tipo=tipo;
+    }
+    
+    public String getDescricao()    {
+        return descricao;
+    }
+    
+    public String getTipo()    {
+        return tipo;
     }
     
 }
